ce209d18a8cd6475bc0c69f15c61b978e2952ae5
rathnayakarmcsl <rathnayakarmcsl@gmail.com>
2026-05-22 18:32:42 +0530

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Ella Sri Lanka Photographer",
+  description: "Luxury photography in Ella Sri Lanka",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}
